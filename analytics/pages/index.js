70767fe511d94c2baac68b84946653f37bfd0043
JoshDesmond <JoshDesmond@users.noreply.github.com>
2022-03-03 12:01:26 -0500

    Setup nextjs project folder

---
+++ analytics/pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Analytics</title>
+        <meta name="description" content="A website for viewing and managing my personal analytics" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        <h1 >
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p >
+          Get started by editing{' '}
+          <code >pages/index.js</code>
+        </p>
+
+      </main>
+
+      <footer >
+        Footer
+      </footer>
+    </div>
+  )
+}
