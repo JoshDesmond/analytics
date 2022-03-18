db8a4185d34ebaa13c65f020fefdb6cd24410f6e
JoshDesmond <JoshDesmond@users.noreply.github.com>
2022-03-18 18:09:50 -0400

    Adds new API endpoint for AP

---
+++ analytics/pages/api/ap/scores.js
@@ -0,0 +1,51 @@
+import Cors from 'cors';
+import http from 'http';
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+
+            return resolve(result);
+        })
+    })
+}
+
+/**
+ * 
+ * @param {http.IncomingMessage} req 
+ * @param {http.ServerResponse} res 
+ */
+export default async function handler(req, res) {
+    // Run the middleware
+    await runMiddleware(req, res, cors);
+
+    const data = JSON.parse(req.body);
+    const method = req.method;
+
+    switch (method) {
+        case 'POST':
+            console.log(data);
+            // TODO move data to database
+            res.status(200).json( `Hello` );
+            break
+        case 'GET':
+            // Get data from your database
+            // TODO how to get params?
+            res.status(200).json({ data: `Hello` });
+            // res.status(200).json({ id, name: `User ${id}` })
+            break
+        default:
+            res.setHeader('Allow', ['GET', 'POST']);
+            res.status(405).end(`Method ${method} Not Allowed`);
+    }
+}
